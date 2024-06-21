3e913944082a3f020fa18f631d09d71df38a29b6
Purushottam <purushottam@bluemint.dev>
2024-06-21 11:34:17 +0530

    change file name of route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/User')
 
 exports.getAllUsers = asyncHandler(async (req, res) => {
-    const result = User.find()
+    const result = await User.find()
     res.status(200).json({ message: "User Fetch success", result })
 })
 
