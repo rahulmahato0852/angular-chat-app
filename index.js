0ce97d27ee4452df1edf0dd6c40bf8805b088813
Purushottam <purushottam@bluemint.dev>
2024-06-22 17:46:47 +0530

    frnt added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const userPortected = require('./middleware/userProtected')
